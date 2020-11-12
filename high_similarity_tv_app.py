435f1e76bce15d352cb3b1cc4771e1a1001d5a0d
han <huhanthu@qq.com>
2020-11-12 11:04:17 +0800

    update copy high similarity apps

--- high_similarity_tv_app.py
+++ high_similarity_tv_app.py
@@ -2,12 +2,9 @@ import os
 import difflib
 import shutil
 
-target_dir = './high_similarity_apps'
+target_dir = 'high_similarity_apps'
 tv_corresponding_apps_path = 'tv_corresponding_apps.txt'
 spacer = '     |||     '
-cmd_instructions = []
-cmd_instructions.append('droidbot -keep_env -is_emulator ')
-cmd_instructions.append('-timeout 30 ')
 
 def check_tv_dic(path):
     file = open(path, 'r')
@@ -18,38 +15,63 @@ def check_tv_dic(path):
         tv_name = line[0]
         an_name = line[1]
         path = line[2]
-
-        tv_name = tv_name.replace("tv", "")
-        tv_name = tv_name.replace("TV", "")
-        tv_name = tv_name.replace("HD", "")
-
-        an_name = an_name.replace("HD", "")
-
-        similarity = difflib.SequenceMatcher(None, tv_name, an_name).quick_ratio()
+        similarity = calculate_similarity(tv_name, an_name)
         if similarity > 0.9:
             #print(line[0]+" "+line[1])
             if not path in paths:
                 paths.append(path)
-            else:
-                print('path:'+path+'have been in paths')
-
+            #else:
+                #print('path:'+path+'have been in paths')
     return paths
 
-def cmd_evoker(path):
-    for root, dirs, files in os.walk(path, topdown=False):
-        for file_path in files:
-            apk_path = root+'/'+file_path
-            print(apk_path)
-            cmd_instructions.append('-a '+apk_path+' ')
-            save_path = 'results'+'/' +file_path
-            print(save_path)
-            cmd_instructions.append('-o '+save_path)
-            cmd = "".join(cmd_instructions)
-            os.system(cmd)
 
 def move_file(path):
-    dir = path[path.rindex('/'):]
-    shutil.copytree(path, target_dir+dir)
+    dir = path[path.rindex('/')+1:]
+    find_high_similarity_apks(dir, path)
+    #shutil.copytree(path, target_dir+dir)
+
+def find_high_similarity_apks(ref, path):
+    for root, dirs, apks in os.walk(path):
+        for apk in apks:
+            similarity = calculate_similarity(ref, apk)
+            print('similarity:'+str(similarity))
+            if similarity > 0.9:
+                print('high similarity: '+apk+' '+ref)
+                src_file = os.path.join(root, apk)
+                dst_dir = os.path.join(target_dir, ref)
+                if not os.path.exists(dst_dir):
+                    os.makedirs(dst_dir)
+                dst_file = os.path.join(dst_dir, apk)
+                shutil.copy(src_file, dst_file)
+
+
+def remove_low_similarity_apks(target_dir):
+    for root, dirs, files in os.walk(target_dir):
+        for dir in dirs:
+            path = os.path.join(root, dir)
+            for root_2, dirs_2, apks in os.walk(path):
+                for apk in apks:
+                    similarity = calculate_similarity(dir, apk)
+                    print('similarity:'+str(similarity))
+                    if similarity<0.9:
+                        print(apk+' '+dir)
+                        remove_file = os.path.join(root_2, apk)
+                        if os.path.exists(remove_file):
+                            os.remove(remove_file)
+
+
+def calculate_similarity(dir, apk):
+    apk = apk.replace("tv_", "")
+    apk = apk.replace("TV", "")
+    apk = apk.replace("HD", "")
+    apk = apk.replace(".apk", "")
+
+    dir = dir.replace("HD", "")
+    dir = dir.replace("tv", "")
+    dir = dir.replace("TV", "")
+    similarity = difflib.SequenceMatcher(None, dir, apk).quick_ratio()
+    return similarity
+
 
 if __name__ == '__main__':
     paths = check_tv_dic(tv_corresponding_apps_path)
@@ -57,4 +79,4 @@ if __name__ == '__main__':
     for path in paths:
         print(path[:-1])
         move_file(path[:-1])
-        #cmd_evoker(path[:-1])
+    remove_low_similarity_apks(target_dir)
